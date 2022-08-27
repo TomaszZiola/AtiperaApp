2bce31a5ecbf79ad74c2eab70f470ddf1c47a6b1
TomaszZiola <tomek.ziola@gmail.com>
2022-08-27 17:24:05 +0200

    Initial commit

---
+++ src/main/java/com/ziola/atiperaapp/AtiperaAppApplication.java
@@ -0,0 +1,13 @@
+package com.ziola.atiperaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiperaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtiperaAppApplication.class, args);
+    }
+
+}
